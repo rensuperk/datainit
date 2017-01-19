c72a4393b696cdba40be860fc6158d7f4dcaa841
renkai <304078213@qq.com>
2017-01-19 14:21:42 +0800

    加了日志和新的同步方法

---
+++ src/main/java/cn/bidlink/nbl/bidding/data/init/InsertPurchaserMain.java
@@ -0,0 +1,54 @@
+package cn.bidlink.nbl.bidding.data.init;
+
+import org.apache.commons.lang3.StringUtils;
+import org.nutz.dao.Chain;
+import org.nutz.dao.Cnd;
+import org.nutz.dao.entity.Record;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.json.JsonLoader;
+
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by renkai on 2017/1/18.
+ * 这个方法是同步项目表中的委托机构的数据，插入purchaser表中
+ */
+public class InsertPurchaserMain {
+
+    public static void main(String[] args) {
+        Ioc ioc = new NutIoc(new JsonLoader("biddingDao.js"));
+        DataSource ds = ioc.get(DataSource.class);
+        NutDao biddingDao = new NutDao(ds);
+        List<Record> list = biddingDao.query("project", Cnd.where("BID_CALL_IDS", "is not", null).and("AUDIT_SATATUS", "=", "3"));
+        for (Record project : list) {
+            String bid_call_ids = project.getString("BID_CALL_IDS");
+            String[] bid_call_idslist = bid_call_ids.split(",");
+            for (String s : bid_call_idslist) {
+                if(StringUtils.isNoneBlank(s)){
+                    Record purchaser = biddingDao.fetch("project_purchaser", Cnd.where("PROJECT_ID", "=", project.get("ID")).and("PROJECT_ID", "=", project.get("ID")));
+                    if(purchaser == null){
+                        Chain make = Chain.make("ID", StringUtils.replace(UUID.randomUUID().toString(),"-",""))
+                                .add("PROJECT_ID",project.get("ID"))
+                                .add("PURCHASER_ID",s)
+                                .add("CREATE_TIME",new Date())
+                                .add("CREATE_USER_ID",project.get("CREATE_USER_ID"))
+                                .add("ORG_CODE",project.get("ORG_CODE"))
+                                .add("TENANT_ID",project.get("TENANT_ID"))
+                                .add("IS_TEST",project.get("IS_TEST"))
+                                .add("SYSTEM_STATUS",project.get("SYSTEM_STATUS"))
+                                ;
+                        biddingDao.insert("project_purchaser",make);
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ src/main/java/cn/bidlink/nbl/bidding/data/init/UpdateCompanyIdMain.java
@@ -0,0 +1,62 @@
+package cn.bidlink.nbl.bidding.data.init;
+
+import org.apache.log4j.Logger;
+import org.nutz.dao.Chain;
+import org.nutz.dao.Cnd;
+import org.nutz.dao.entity.Record;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.json.JsonLoader;
+
+import javax.sql.DataSource;
+import java.util.*;
+
+/**
+ * Created by renkai on 2017/1/19.
+ */
+public class UpdateCompanyIdMain {
+    public static Logger logger = Logger.getLogger(UpdateCompanyIdMain.class);
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Ioc ioc = new NutIoc(new JsonLoader("biddingDao.js"));
+        DataSource ds = ioc.get(DataSource.class);
+        NutDao biddingDao = new NutDao(ds);
+
+        Ioc userioc = new NutIoc(new JsonLoader("userDao.js"));
+        DataSource userds = userioc.get(DataSource.class);
+        NutDao userDao = new NutDao(userds);
+        Set<String> projectSet = new HashSet<String>();
+        Map<String, Object> orgMap = new HashMap<String, Object>();
+        update("project_purchaser",biddingDao,userDao,projectSet,orgMap);
+    }
+    public static  void  update(String tableName,NutDao biddingDao,NutDao userDao,Set<String> key,Map<String, Object> nameMap){
+        List<Record> purchaser_project = biddingDao.query(tableName, Cnd.where("TENANT_ID", "is not", null));
+        int i = 0;
+        for (Record record : purchaser_project) {
+            String tenant_id = record.getString("TENANT_ID");
+            Object company_id = record.get("COMPANY_ID");
+//            logger.info("查询"+company_id+" | "+tenant_id);
+            if(company_id == null){
+                if(!nameMap.containsKey(tenant_id)) {
+                    Record record1 = userDao.fetch("user", Cnd.where("ID", "=", tenant_id));
+                    if (record1 != null) {
+                        nameMap.put(tenant_id, record1.get("OLD_COMP_ID"));
+                    }
+                }
+                company_id = nameMap.get(tenant_id);
+                if(company_id == null){
+                    continue;
+                }
+                if(!key.contains(company_id+tenant_id)){
+                    biddingDao.update(tableName,
+                            Chain.make("COMPANY_ID",company_id),
+                            Cnd.where("TENANT_ID", "=", tenant_id));
+                    i++;
+                    logger.info("更新"+i+"次:"+company_id+" | "+tenant_id);
+                    key.add(company_id+tenant_id);
+                }
+            }
+        }
+    }
+}
